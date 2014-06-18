944678ee1605e350086cba02ae871b13f79825fd
Shane Nishioka-Healy <shanenh18@gmail.com>
2014-06-17 23:50:49 -1000

    Exam_2

--- Exam2/Exam2/Source.cpp
+++ Exam2/Exam2/Source.cpp
@@ -24,16 +24,47 @@ int main(){
 	}
 	reg.close();
 
+	//Write Runners to appropriate color text file
+	ofstream wh, ye, gr, or, bl, li, re;
+	wh.open("white.txt");
+	ye.open("yellow.txt");
+	gr.open("green.txt");
+	or.open("orange.txt");
+	bl.open("blue.txt");
+	li.open("lilac.txt");
+	re.open("red.txt");
+	for (int a = 0; a < 1001; a++){
+			if (runner[a].pace <= 360)
+				wh << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
 
-	int temp = 1000;
-	int i = 0;
-	int a = 0;
-	const int c = 2000;
-	int tarray[c];
-	if (runner[i].pace < temp){
-		runner[i].pace = temp;
-		runner[i].pace = tarray[a];
-		i++;
-		a++;
-	}
+			 if (runner[a].pace >= 361 && runner[a].pace <= 420)
+				ye << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+
+			 if (runner[a].pace >= 421 && runner[a].pace <= 480)
+				gr << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+
+			 if (runner[a].pace >= 481 && runner[a].pace <= 540)
+				or << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+
+			 if (runner[a].pace >= 541 && runner[a].pace <= 600)
+				bl << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+
+			 if (runner[a].pace >= 601 && runner[a].pace <= 720)
+				li << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+
+			 if (runner[a].pace >= 721 && runner[a].pace <= 1200)
+				re << runner[a].firstname << " " << runner[a].lastname << " " << runner[a].pace << endl;
+			
+				
+		}
+
+	wh.close();
+	ye.close();
+	gr.close();
+	or.close();
+	bl.close();
+	li.close();
+	re.close();
 }
+
+
